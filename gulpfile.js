221c23bd4ec0dff65a8e11faa579dbec9eb27ec5
uzunhair <uzunhair@yandex.ru>
2019-06-20 13:26:49 +0300

    Старт

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require("gulp"),
+    reqDir = require('require-dir');
+    reqDir('./gulp', { recurse: true });
+
+gulp.task('default', function(cb) {
+    gulp.parallel(
+      'build',
+      'watch'
+    )(cb)
+});
